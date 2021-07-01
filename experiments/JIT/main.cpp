4e34afd499c9aeee62139acded0f3b333915e910
zzb66666666x <18767066589@163.com>
2021-07-01 17:02:57 +0800

    updated

---
+++ experiments/JIT/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <libtcc.h>
+#include "support.h"
+
+const char* program = \
+    "#include <tcclib.h>"
+    "#include \"support.h\""
+    "SHADER_IN_PORT mydata;"
+    "void foo(SHADER_IN_PORT input){"
+    "   mydata = input;"
+    "}"
+    "void disp(){"
+    "   printf(\"%f, %f, %f\", mydata.x, mydata.y, mydata.z);"
+    "}"
+    ;
+
+typedef void (*fun_ptr)(SHADER_IN_PORT);
+typedef void (*fun_ptr2)(void);
+
+int main(int argc, char **argv)
+{
+    TCCState* s = tcc_new();
+    if(!s){
+        printf("Can’t create a TCC context\n");
+        return 1;
+    }
+    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+   
+    if (tcc_compile_string(s, program) > 0) {
+        printf("Compilation error !\n");
+        return 2;
+    }
+
+    tcc_relocate(s, TCC_RELOCATE_AUTO);
+
+    SHADER_IN_PORT port_data;
+    port_data.x = 1;
+    port_data.y = 2;
+    port_data.z = 3;
+    fun_ptr foo = (fun_ptr)tcc_get_symbol(s, "foo");
+    foo(port_data);
+
+    fun_ptr2 disp = (fun_ptr2)tcc_get_symbol(s, "disp");
+    disp();
+   
+    tcc_delete(s);
+    return 0;
+}
---
+++ experiments/JIT/support.h
@@ -0,0 +1,10 @@
+#ifndef _SUPPORT_H
+#define _SUPPORT_H
+
+typedef struct{
+float x;
+float y;
+float z;
+}SHADER_IN_PORT;
+
+#endif
