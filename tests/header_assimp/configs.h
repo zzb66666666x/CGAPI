51cf897b061e99a64f65ac8acddc8ecea28406bf
zzb66666666x <18767066589@163.com>
2021-07-27 16:22:47 +0800

    updated

---
+++ tests/header_assimp/configs.h
@@ -0,0 +1,7 @@
+#ifndef CONFIGS_H
+#define CONFIGS_H
+
+#define GLM_FORCE_AVX2
+#define GLM_FORCE_INLINE 
+
+#endif
---
+++ tests/test_prog_sponza_texture.cpp
@@ -0,0 +1,3 @@
+#include "header_assimp/configs.h"
+#include "header/model.h"
+#include "header_assimp/shader.h"
